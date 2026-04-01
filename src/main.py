8e673139335b079c89abac332bf0f69be44853aa
Nycollas-moretz <nycomoretz@gmail.com>
2026-04-01 09:24:23 -0300

    implementação da deteção de velocidade do movimento apenas da mão direita

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ import cv2
 from video.capture import VideoCapture
 from pose.yolo_pose import YoloPose
 from processing.key_points import KeyPointsExtractor
+from processing.motion import MotionAnalyzer
+
 def main():
 
     #inicializa captura
@@ -17,6 +19,11 @@ def main():
     key_points_extractor = KeyPointsExtractor()
     keypoints = None
     
+    #inicializa analisador de movimento
+    motion_analyzer = MotionAnalyzer()
+
+    #contador de frames
+    frame_count = 0
     while True:
         
         #pega o frame da webcam / video
@@ -32,14 +39,25 @@ def main():
         keypoints = key_points_extractor.extract(results)
         right_hand = key_points_extractor.get_right_hand(keypoints)
 
+        #analisa o movimento da mão direita
+        movement = motion_analyzer.compute_movement(right_hand)
+
+        if movement is not None:
+            dx, dy = movement
+            if frame_count % 5 == 0: #printa a cada 10 frames para não poluir o console
+                print(f"Right hand movement: dx={dx}, dy={dy}")
+
         if right_hand is not None:
-            print(f"Right hand key point: {right_hand}")
+             if frame_count % 10 == 0: #printa a cada 10 frames para não poluir o console
+                
+                print(f"Right hand key point: {right_hand}")
             
         cv2.imshow("Pose Detection", annotated_frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    
+        
+        frame_count += 1
     #libera a captura e fecha as janelas
     video_capture.release()
     cv2.destroyAllWindows()
--- src/processing/motion.py
+++ src/processing/motion.py
@@ -1,1 +1,26 @@
 #vai analisar o movimento, para que possamos identificar os golpes, e para mostrar na tela, para que possamos vizualizar o que o modelo está vendo, e para mostrar as informações de cada boxeador
+
+class MotionAnalyzer:
+    def __init__(self):
+        #vai guardar pos anterior
+        self.prev_right_hand = None
+
+    def compute_movement(self, current_right_hand):
+        #calcula movimento da mao direita
+
+        if current_right_hand is None:
+            return None
+        
+        if self.prev_right_hand is None:
+            #primeira execução, não tem movimento
+            self.prev_right_hand = current_right_hand
+            return None
+        
+        #calcula a diferença entre a posição atual e a anterior
+        dx = current_right_hand[0] - self.prev_right_hand[0]
+        dy = current_right_hand[1] - self.prev_right_hand[1]
+
+        #atualiza a posição anterior
+        self.prev_right_hand = current_right_hand
+
+        return (dx, dy)
